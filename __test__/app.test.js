ee102233261674b70229c27b1281238134e429b7
Katherine Tam <katherinemtam@gmail.com>
2021-06-10 16:20:12 -0700

    dogs post route test

---
+++ __test__/app.test.js
@@ -0,0 +1,36 @@
+import pool from '../lib/utils/pool.js';
+import setup from '../data/setup.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+
+// CRUD
+// C - create   POST      INSERT
+// R - read     GET       SELECT
+// U - update   PUT       UPDATE
+// D - delete   DELETE    DELETE
+
+describe('dog routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  test('create a dog via POST', async () => {
+    const res = await request(app)
+      .post('/api/v1/dogs')
+      .send({
+        name: 'spot',
+        age: 5,
+        weight: '20 lbs'
+      });
+
+    
+  });
+
+
+
+
+
+
+
+
+});
