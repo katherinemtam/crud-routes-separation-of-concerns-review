e1ad42dfe727583a56411e4f5b13113de3b1d79a
Katherine Tam <katherinemtam@gmail.com>
2021-06-11 15:06:52 -0700

    birds get (all) route test

--- __test__/app-birds.test.js
+++ __test__/app-birds.test.js
@@ -2,13 +2,14 @@ const pool = require('../lib/utils/pool.js');
 const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
+const Bird = require('../lib/models/Bird.js');
 
 describe('bird routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  test('create a dog via POST', async () => {
+  test('create a bird via POST', async () => {
     const res = await request(app)
       .post('/api/v1/birds')
       .send({
@@ -21,5 +22,27 @@ describe('bird routes', () => {
       type: 'kiwi',
       origin: 'australia'
     });
+  });
+
+  test('get all birds via GET', async () => {
+    const kiwi = await Bird.insert({
+      type: 'kiwi',
+      origin: 'australia'
+    });
+
+    const ostrich = await Bird.insert({
+      type: 'ostrich',
+      origin: 'africa'
+    });
+
+    const penguin = await Bird.insert({
+      type: 'penguin',
+      origin: 'antarctica'
+    });
+    
+    const res = await request(app)
+      .get('/api/v1/birds');
+
+    expect(res.body).toEqual([kiwi, ostrich, penguin]);
   });
 });
