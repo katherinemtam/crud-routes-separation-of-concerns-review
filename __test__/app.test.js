c25cd30db75c353c3f552a6c11fbb6544c268f76
Katherine Tam <katherinemtam@gmail.com>
2021-06-10 21:05:19 -0700

    dogs get (all) route test

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Dog from '../lib/models/Dog.js';
 
 // CRUD
 // C - create   POST      INSERT
@@ -29,6 +30,31 @@ describe('dog routes', () => {
       age: 5,
       weight: '20 lbs'
     });
+  });
+
+  test('finds all dogs via GET', async () => {
+    const spot = await Dog.insert({
+      name: 'spot',
+      age: 5,
+      weight: '20 lbs'
+    });
+
+    const willow = await Dog.insert({
+      name: 'willow',
+      age: 3,
+      weight: '25 lbs'
+    });
+    
+    const xe = await Dog.insert({
+      name: 'xe',
+      age: 7,
+      weight: '80 lbs'
+    });
+    
+    const res = await request(app)
+      .get('/api/v1/dogs');
+
+    expect(res.body).toEqual([spot, willow, xe]);
     
   });
 
