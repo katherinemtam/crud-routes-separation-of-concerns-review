c97c05e7692550638250c71a74eb3f632bdbebe1
Katherine Tam <katherinemtam@gmail.com>
2021-06-13 21:38:57 -0700

    teas get (all) route test

--- __test__/app-teas.test.js
+++ __test__/app-teas.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool.js');
 const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
+const Tea = require('../lib/models/Tea.js');
 
 describe('tea routes', () => {
   beforeEach(() => {
@@ -33,5 +34,42 @@ describe('tea routes', () => {
       brewTimeMinutesMax: 4,
       hasSugar: false
     });
+  });
+
+  test('get all teas via GET', async () => {
+    const chai = await Tea.insert({
+      name: 'chai tea',
+      type: 'black',
+      origin: 'india',
+      brewTempF: 195,
+      brewTimeMinutesMin: 3,
+      brewTimeMinutesMax: 4,
+      hasSugar: true
+    });  
+
+    const sweet = await Tea.insert({
+      name: 'sweet tea',
+      type: 'black',
+      origin: 'usa',
+      brewTempF: 195,
+      brewTimeMinutesMin: 3,
+      brewTimeMinutesMax: 4,
+      hasSugar: true
+    });  
+
+    const monkey = await Tea.insert({
+      name: 'monkey picked white tea',
+      type: 'white',
+      origin: 'china',
+      brewTempF: 175,
+      brewTimeMinutesMin: 4,
+      brewTimeMinutesMax: 5,
+      hasSugar: false
+    });  
+
+    const res = await request(app)
+      .get('/api/v1/teas');
+
+    expect(res.body).toEqual([chai, sweet, monkey]);
   });
 });
